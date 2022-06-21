a2b719448c057d99d31268f45fe44d84b4c3deac
Carly Hagen <carlyrhagen@gmail.com>
2022-06-21 14:47:45 -0400

    cleaned up default app, added gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
-
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Module msg="You did it!"/>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -25,6 +12,13 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
   <RouterView />
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Module from "@/components/Module.vue";
+
+
+</script>
 
 <style>
 @import "@/assets/base.css";
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <h1> header1 </h1>
+  </main>
+</template>
+
+<script>
+
+</script>
