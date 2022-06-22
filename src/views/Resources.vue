a15fa64e68d5496f12807ff6e59cbd43e0dc6058
Carly Hagen <carlyrhagen@gmail.com>
2022-06-21 20:03:12 -0400

    added d3

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="view">
     <h1>This is the resource page.</h1>
+    <PackChart></PackChart>
   </div>
 </template>
+
+<script>
+import PackChart from "@/components/Packchart.vue"
+
+export default {
+  name: "Resources",
+  components: { PackChart }
+}
+
+</script>
