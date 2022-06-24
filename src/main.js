d3682243644d52ec0567debea1af73d0e70b44dd
Carly Hagen <carlyrhagen@gmail.com>
2022-06-24 12:39:36 -0400

    rolled back test libraries

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from "vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueCytoscape from 'vue-cytoscape'
-
-Vue.use(router);
 
 const app = createApp(App);
 app.use(router);
--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="view">
     <h1>This is the resource page.</h1>
-    <PackChart></PackChart>
   </div>
 </template>
 
 <script>
-import PackChart from "@/components/Packchart.vue"
 
 export default {
   name: "Resources",
-  components: { PackChart }
+  components: {  }
 }
 
 </script>
