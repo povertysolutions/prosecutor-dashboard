2f023e1212452a0aa84f50c4502bc083cdfe43aa
Carly Hagen <carlyrhagen@gmail.com>
2022-06-24 18:16:57 -0400

    chart.js added and bar chart working

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="view">
     <h1>This is the dashboard.</h1>
+    <LineGraph></LineGraph>
   </div>
 </template>
+
+<script>
+import LineGraph from "@/components/LineGraph"
+
+export default {
+  name: "Resources",
+  components: { LineGraph }
+}
+
+</script>
