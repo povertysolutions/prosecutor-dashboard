ebe30e49e47276590e78396c9e9cd347cffa6bb0
Carly Hagen <carlyrhagen@gmail.com>
2022-07-20 13:32:57 -0400

    added content to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,44 @@
 <template>
-  <div class="view">
-    <h2>This is the about page.</h2>
-  </div>
+  <div class="about">
+     <h2>{{content["intro-header"][lang]}}</h2>
+     <p>{{content["intro-body"][lang]}}</p>
+     <hr>
+     <h2>{{content["background-header"][lang]}}</h2>
+     <p>{{content["background-body"][lang]}}</p>
+     <hr>
+     <h2>{{content["justice-system-header"][lang]}}</h2>
+     <p>{{content["justice-system-body"][lang]}}</p>
+   </div>
 </template>
 
 <script>
+import content from "../../assets/about.json"
+
 export default {
-  name: "About"
+  name: "About",
+  data(){
+    return{
+      lang: "en",
+      content: content
+    }
+  }
 }
 
 </script>
 
 <style lang="scss" scoped>
-.view{
-  position: fixed;
-  margin: 5rem 2rem;
+@import "@/styles/main.scss";
+.about{
+  position: absolute;
+  margin: 5rem 10rem;
+  padding: 2rem;
+  min-width: 500px;
+  //border: 1px blue solid;
 }
+
+hr{
+  margin: 2rem 0;
+}
+
 
 </style>
